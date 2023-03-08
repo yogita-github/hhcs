859587c1d857a7d19b1550da68b39d05a2200aaa
yogita-github <103029645+yogita-github@users.noreply.github.com>
2023-03-08 20:35:05 +0530

    new changes

--- hhcs new/main.py
+++ hhcs new/main.py
@@ -17,21 +17,73 @@ def index():
 @app.route('/submit', methods=['POST'])
 def forms():
     gender = request.form['gender']
+    # if (gender == 'male'):
+    #     gender=0
+    # else:
+    #     gender=1
     age = request.form['age']
-    sbp = request.form['SBP']
-    hbp = request.form['HBP']
+    sbp = request.form['sbp']
+    hbp = request.form['hbp']
     spo2 = request.form['spo2']
     temp = request.form['temp']
     h_rate = request.form['h_rate']
     glc = request.form['glc']
     res = predict(gender,age, sbp, hbp, h_rate, glc, spo2, temp)
-    print(res)
-    return render_template('forms.html')
+    # print(gender)
+    # print(spo2)
+    # print(sbp)
+    # print(hbp)
+    # print(h_rate)
+    # print(glc)
+    if (res[0] == 1):
+        res = "healthy"
+    elif(res[0] == 2):
+        res = "High BP"
+    elif(res[0] == 3):
+        res = "LOW BP"
+    elif(res[0] == 4):
+        res = "High Sugar"
+    elif(res[0] == 5):
+        res = "Low Sugar"
+    elif(res[0] == 6):
+        res = "Low Oxygen"
+    elif(res[0] == 7):
+        res = "High Temperature"
+    elif(res[0] == 8):
+        res = "Heartbeat is High"
+    elif(res[0] == 9):
+        res = "Risk"
+    
+
+    return render_template('forms.html', res = res)
 
 @app.route('/forms', methods=['POST', 'GET'])
 def form():
     return render_template('forms.html')
 
+@app.route('/tips', methods=['POST', 'GET'])
+def tips():
+    return render_template('tips.html')
+
+@app.route('/posts', methods=['POST', 'GET'])
+def posts():
+    return render_template('post.html')
+
+@app.route('/about', methods=['POST', 'GET'])
+def about():
+    return render_template('about.html')
+
+@app.route('/recommendations', methods=['POST', 'GET'])
+def recommend():
+    return render_template('recommendations.html')
+
+@app.route('/faqs', methods=['POST', 'GET'])
+def faqs():
+    return render_template('faq.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def logi():
+    return render_template('login.html')
 
 def predict(g,a,s,h,hr,gl,sp,temp):
     p = [[g,a,s,h,hr,gl,sp,temp],
